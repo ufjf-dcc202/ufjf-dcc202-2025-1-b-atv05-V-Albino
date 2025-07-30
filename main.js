a8b5d772f9382cd26590760d9d689370cea13e2b
V-Albino <victorbrandao007@gmail.com>
2025-07-30 16:48:26 -0300

    Commit 4 - Adiciona criaDisco

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ eH1.textContent = "Olá mundo!";
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
 
+for (let i = 0; i < 7; i++) {
+    const eDisco = criaDisco();
+    eTabuleiro.append(eDisco);
+}
+
+function criaDisco(){
+    const novoDisco = document.createElement("div");
+    novoDisco.classList.add("disco");
+    return novoDisco
+}
+
 function criaTabuleiro(){
     const novoTabuleiro = document.createElement("div");
     novoTabuleiro.classList.add("tabuleiro");
